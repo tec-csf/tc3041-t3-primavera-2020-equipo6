f9cdfd12ebcdff31e09877b1938be9ab2ba4421d
Semy Levy <semylb@gmail.com>
2020-04-20 11:31:55 -0500

    Fix foreign ids, generate new data

--- database/gendata.py
+++ database/gendata.py
@@ -168,7 +168,7 @@ def genForeignIds(idRange, maxIds):
   res = []
   for i in range(n):
     index = random.randint(0,idRange-1)
-    res.append(str(index))
+    res.append(index)
   return res
 
 def writeToFile(filename, fields, rows):
@@ -229,7 +229,7 @@ def insertToDb(collection, document):
   collection.insert_many(document)
 
 def main():
-  db = connectToDb("")
+  db = connectToDb("x3jPieXWD5lEXmPr")
 
   filename = "gen/actores.json"
   actores = genActor(100000)
