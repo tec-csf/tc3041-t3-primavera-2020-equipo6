75ca8c97b3f02e8e6238fe81da2205b8cbadd61e
Semy Levy <semylb@gmail.com>
2020-04-20 14:32:41 -0500

    Add more queries

--- database/aggregation.js
+++ database/aggregation.js
@@ -95,3 +95,116 @@
     }
   }
 ]
+
+// Returns movies where specified actor appears
+[
+  {
+    '$lookup': {
+      'from': 'actor', 
+      'localField': 'id_actor', 
+      'foreignField': '_id', 
+      'as': 'actores'
+    }
+  }, {
+    '$unwind': {
+      'path': '$actores'
+    }
+  }, {
+    '$match': {
+      'actores.nombre': 'kirch sinoussi'
+    }
+  }, {
+    '$group': {
+      '_id': '$actores._id', 
+      'nombre': {
+        '$first': '$actores.nombre'
+      }, 
+      'peliculas': {
+        '$addToSet': '$nombre'
+      }, 
+      'pais': {
+        '$first': '$actores.pais'
+      }
+    }
+  }, {
+    '$sort': {
+      'pais': 1
+    }
+  }
+]
+
+// Returns movie showtimes ordered by location and date
+[
+  {
+    '$geoNear': {
+      'near': {
+        'type': 'Point', 
+        'coordinates': [
+          -118.266743, 34.0582395
+        ]
+      }, 
+      'distanceField': 'ubicacion.calculated', 
+      'query': {}
+    }
+  }, {
+    '$lookup': {
+      'from': 'sala', 
+      'localField': 'id_sala', 
+      'foreignField': '_id', 
+      'as': 'salas'
+    }
+  }, {
+    '$unwind': {
+      'path': '$salas'
+    }
+  }, {
+    '$lookup': {
+      'from': 'proyeccion', 
+      'localField': 'salas.id_proyeccion', 
+      'foreignField': '_id', 
+      'as': 'proyecciones'
+    }
+  }, {
+    '$unwind': {
+      'path': '$proyecciones'
+    }
+  }, {
+    '$unwind': {
+      'path': '$proyecciones.id_pelicula'
+    }
+  }, {
+    '$lookup': {
+      'from': 'pelicula', 
+      'localField': 'proyecciones.id_pelicula', 
+      'foreignField': '_id', 
+      'as': 'pelicula'
+    }
+  }, {
+    '$unwind': {
+      'path': '$pelicula'
+    }
+  }, {
+    '$project': {
+      '_id': 0, 
+      'cine': '$nombre', 
+      'horario': {
+        '$toDate': {
+          '$multiply': [
+            '$proyecciones.horario', 1000
+          ]
+        }
+      }, 
+      'numero sala': '$salas.numero', 
+      'numero de asientos': '$salas.asientos', 
+      'tipo de sala': '$salas.tipo', 
+      'precio': '$proyecciones.precio', 
+      'nombre': '$pelicula.nombre'
+    }
+  }, {
+    '$limit': 1000
+  }, {
+    '$sort': {
+      'horario': 1
+    }
+  }
+]
